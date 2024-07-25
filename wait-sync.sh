de6550bb0eb0652aa5c847b9dfce314a9ce04dff
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-07-25 18:23:08 +0300

    wait sync check node is running

--- wait-sync.sh
+++ wait-sync.sh
@@ -3,6 +3,13 @@
 LOGPFX=$(basename "$0"):
 cd "$(dirname "$(realpath "$0")")"
 
+source _common.sh
+
+if ! is_node_running; then
+    echo "$LOGPFX: Node is not running"
+    exit 1
+fi
+
 echo -n "$LOGPFX Waiting"
 
 while ! ./goal.sh node status | grep -q 'Sync Time: 0.0s'; do
