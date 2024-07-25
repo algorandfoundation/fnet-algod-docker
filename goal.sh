814a471e5bfa2c46fcabdcf30434688df4531941
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-07-25 17:33:12 +0300

    goal.sh make compatible with stdin pipe

--- goal.sh
+++ goal.sh
@@ -2,4 +2,11 @@
 
 cd "$(dirname "$(realpath "$0")")"
 
-DOCKER_CLI_HINTS=false docker exec -it node-fnet /node/goal -d /node/data "$@"
+flags="-it"
+
+# test if stdin has anything; remove the -t flag if so
+if read -t 0; then
+    flags="-i"
+fi
+
+DOCKER_CLI_HINTS=false exec docker exec $flags node-fnet /node/goal -d /node/data "$@"
