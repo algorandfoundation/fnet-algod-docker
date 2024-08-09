e84e50be66015332d5fff29883a3de545940c929
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-08-09 13:04:06 +0300

    goal.sh remove tty flag from docker exec

--- goal.sh
+++ goal.sh
@@ -2,11 +2,4 @@
 
 cd "$(dirname "$(realpath "$0")")"
 
-flags="-it"
-
-# test if stdin has anything; remove the -t flag if so
-if read -t 0; then
-    flags="-i"
-fi
-
-DOCKER_CLI_HINTS=false exec docker exec $flags node-fnet /node/goal -d /node/data "$@"
+DOCKER_CLI_HINTS=false exec docker exec -i node-fnet /node/goal -d /node/data "$@"
