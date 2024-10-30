b0ae0cf7b7daa300ead737ab6f4ad2dbe848216c
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-10-30 16:53:22 +0100

    Fix update checker, trailing \r\n vs \n was breaking the check when we were on the latest version

--- utils/get_local_image_digest.sh
+++ utils/get_local_image_digest.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$(realpath "$0")")/.."
 
 source utils/_common.sh
 
-docker image inspect "$DOCKER_IMAGE_TAG" | jq -r '.[0].Id'
+docker image inspect "$DOCKER_IMAGE_TAG" | jq -r '.[0].RepoDigests[0]' | cut -d@ -f2- | tr -d '\r\n'
